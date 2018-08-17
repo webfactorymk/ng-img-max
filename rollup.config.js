e46a988fa6d683c34fc5e479d7d7d6050e9a23c2
Snezhana Dichevska <snezhana.dichevska@webfactory.mk>
2018-08-17 09:42:54 +0200

    Rename dist files

--- rollup.config.js
+++ rollup.config.js
@@ -1,9 +1,9 @@
 export default {
-  entry: 'dist/ng2-img-max.js',
-  dest: 'dist/bundles/ng2-img-max.umd.js',
+  entry: 'dist/ng2-img-max-cl.js',
+  dest: 'dist/bundles/ng2-img-max-cl.umd.js',
   sourceMap: false,
   format: 'umd',
-  moduleName: 'ng2-img-max',
+  moduleName: 'ng2-img-max-cl',
   globals: {
     '@angular/core': 'ng.core',
     'rxjs': 'Rx'
